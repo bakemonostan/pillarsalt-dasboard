3bca0be4fcb20430a95b9b7a64d4cccf50af8f9b
bakemonostan <ehizomokhomion@gmail.com>
2023-07-23 01:18:21 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    NodeGlobalsPolyfillPlugin({
-      buffer: true,
-    }),
-    nodePolyfills(),
-  ],
+  plugins: [react()],
 });
